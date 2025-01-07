af9597143bf6cd398cf50909353b5102de7d9fdc
Gaurav <gauravvasdev17@gmail.com>
2025-01-07 23:22:00 +0530

    model base set up

--- user-service/src/main/resources/db/changelog/changes/R__create_user_table.sql
+++ user-service/src/main/resources/db/changelog/changes/R__create_user_table.sql
@@ -1,9 +1,14 @@
 --liquibase formatted sql
 
--- changeset liquibase:1
+-- changeset liquibase:1 runOnChange:true
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
+
+-- changeset liquibase:2 runOnChange:true
+
+INSERT INTO users(username, email) values('A', 'A@gmail.com');
+INSERT INTO users(username, email) values('B', 'B@gmail.com');
